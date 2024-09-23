9e4d52cf0bfe8132ac4f2dbab4dc3ba834c79b60
TekMath <matheo.coquet@epitech.eu>
2024-09-23 23:51:49 +0200

    feat: add payload type for packet header

--- libs/network/src/tools/packet/header.hpp
+++ libs/network/src/tools/packet/header.hpp
@@ -15,13 +15,29 @@ constexpr std::uint8_t kPacketHeaderFlagSize = 1;
 
 namespace rtype::sdk::network::tools {
   /**
+   * @brief Payload types for the packet header
+   */
+  enum class PayloadType {
+    kCustom = 0,
+    kCharacter = 1,
+    kInt8 = 2,
+    kUint8 = 3,
+    kInt16 = 4,
+    kUint16 = 5,
+    kInt32 = 6,
+    kUint32 = 7,
+    kFloat = 8,
+    kDouble = 9,
+  };
+
+  /**
    * @brief Packet header properties
    * @warning Do not exceed the maximum values delimited in bits
    */
   struct PacketHeaderProps {
-    std::uint16_t payloadLength = 0;    // 10 bits, do not exceed 1023
-    std::uint8_t payloadType = 0;       // 4 bits, do not exceed 15
-    bool offsetFlag = false;            // 1 bit
-    bool turnFlag = false;              // 1 bit
+    std::uint16_t payloadLength = 0;                // 10 bits, do not exceed 1023
+    PayloadType payloadType = PayloadType::kCustom; // 4 bits, do not exceed 15
+    bool offsetFlag = false;                        // 1 bit
+    bool turnFlag = false;                          // 1 bit
   };
 }
