4764ce70dc2f37b50fa5aadf5c1b04c9f2cd59b0
Matheo Coquet <matheo.coquet@epitech.eu>
2024-09-23 16:31:37 +0200

    docs: update footer description

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} G-Epitech, Inc.`,
     },
     prism: {
       theme: prismThemes.github,
