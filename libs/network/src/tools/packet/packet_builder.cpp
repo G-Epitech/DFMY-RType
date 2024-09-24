9917c665c3db047f788b302d52b7ee48b374cce0
TekMath <matheo.coquet@epitech.eu>
2024-09-24 16:24:19 +0200

    feat: add base of packer builder

--- libs/network/src/tools/packet/packet_builder.cpp
+++ libs/network/src/tools/packet/packet_builder.cpp
@@ -6,3 +6,55 @@
 */
 
 #include "packet_builder.hpp"
+
+using namespace rtype::sdk::network;
+
+tools::PacketBuilder::PacketBuilder() = default;
+
+tools::PacketBuilder::~PacketBuilder() {
+  this->Reset();
+};
+
+tools::PacketBuilder::Exception::Exception(std::string message) : mMessage(std::move(message)) {}
+
+const char *tools::PacketBuilder::Exception::what() const noexcept {
+  return ("PacketBuilder exception: " + mMessage).c_str();
+}
+
+void tools::PacketBuilder::Reset() {
+  mHeader = PacketHeaderProps();
+  mMessage = PacketMessageProps();
+  mOffset = PacketOffsetProps();
+  mTurn = PacketTurnProps();
+}
+
+tools::PacketBuilder &tools::PacketBuilder::SetPayloadType(tools::PayloadType payloadType) {
+  auto castType = static_cast<unsigned>(payloadType);
+
+  if (!IsValidBitSize(castType, kPacketHeaderPayloadTypeSize))
+    throw Exception("Invalid argument: Payload type is out of range.");
+
+  this->mHeader.payloadType = castType;
+  return *this;
+}
+
+tools::PacketBuilder &tools::PacketBuilder::SetTurn(std::uint16_t turn) {
+  if (!IsValidBitSize(turn, kPacketTurnSize))
+    throw Exception("Invalid argument: Turn is out of range.");
+
+  this->mTurn.turn = turn;
+  this->mHeader.turnFlag = 1;
+  return *this;
+}
+
+tools::PacketBuilder &tools::PacketBuilder::SetMessageType(std::uint8_t messageType) {
+  if (!IsValidBitSize(messageType, kPacketMessageTypeSize))
+    throw Exception("Invalid argument: Message type is out of range.");
+
+  this->mMessage.messageType = messageType;
+  return *this;
+}
+
+bool tools::PacketBuilder::IsValidBitSize(unsigned int value, std::uint8_t bitSize) {
+  return value < pow(2, bitSize);
+}
--- libs/network/src/tools/packet/packet_builder.hpp
+++ libs/network/src/tools/packet/packet_builder.hpp
@@ -7,6 +7,80 @@
 
 #pragma once
 
-class PacketBuilder {
+#include "api.hpp"
+#include "packet.hpp"
 
+namespace rtype::sdk::network::tools {
+  class EXPORT_NETWORK_SDK_API PacketBuilder;
+}
+
+class EXPORT_NETWORK_SDK_API rtype::sdk::network::tools::PacketBuilder {
+public:
+  PacketBuilder();
+
+  ~PacketBuilder();
+
+  /**
+   * @brief Packet builder exceptions class
+   */
+  class Exception : public std::exception {
+  public:
+    /**
+     * @brief Create a new message when packet builder exception
+     * @param message The message
+     */
+    explicit Exception(std::string message);
+
+    /**
+     * @brief Get the exception message
+     * @return The exception message
+     */
+    [[nodiscard]] const char *what() const noexcept override;
+
+  private:
+    /// @brief The exception message
+    std::string mMessage;
+  };
+
+  /**
+   * @brief Reset the packet builder
+   */
+  void Reset();
+
+  /**
+   * @brief Set the type of the payload
+   * @param payloadType The payload type
+   */
+  PacketBuilder &SetPayloadType(PayloadType payloadType);
+
+  /**
+   * @brief Set turn (It will enable turn flag)
+   * @param turn The turn (max 65535)
+   */
+  PacketBuilder &SetTurn(std::uint16_t turn);
+
+  /**
+   * @brief Set the message type
+   * @param messageId The message type
+   * @warning The message type must be between 0 and 63.
+   */
+  PacketBuilder &SetMessageType(std::uint8_t messageType);
+
+private:
+  /**
+   * @brief Check if the value is valid for the bit size
+   * @param value The value to check
+   * @param bitSize The bit size
+   * @return True if the value is valid, false otherwise
+   */
+  [[nodiscard]] static bool IsValidBitSize(unsigned value, std::uint8_t bitSize);
+
+  /// @brief Packet header properties
+  PacketHeaderProps mHeader;
+  /// @brief Packet message properties
+  PacketMessageProps mMessage;
+  /// @brief Packet offset properties
+  PacketOffsetProps mOffset;
+  /// @brief Packet turn properties
+  PacketTurnProps mTurn;
 };
