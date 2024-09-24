4184e26e781a487ddc9e2416ac0f6f1ac61e2161
TekMath <matheo.coquet@epitech.eu>
2024-09-24 10:34:07 +0200

    fix: network sdk lib rename and remove invalid functions on client/server

--- client/dependencies.cmake
+++ client/dependencies.cmake
@@ -3,4 +3,4 @@ find_package(fmt REQUIRED)
 target_link_libraries(${CLIENT_CORE_LIB} PRIVATE fmt::fmt)
 
 # Internal dependencies
-target_link_libraries(${CLIENT_CORE_LIB} PRIVATE r-type_network_common)
+target_link_libraries(${CLIENT_CORE_LIB} PRIVATE r-type_network_sdk)
