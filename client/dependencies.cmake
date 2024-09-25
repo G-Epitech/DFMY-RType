9bf081c6b2692513d4477bb6658fbab51470b9c0
sdragos1 <dragos.suceveanu@epitech.eu>
2024-09-25 11:49:50 +0200

    build: fix windows build with sfml

--- client/dependencies.cmake
+++ client/dependencies.cmake
@@ -1,8 +1,8 @@
 # External dependencies
 find_package(fmt REQUIRED)
 target_link_libraries(${CLIENT_CORE_LIB} PRIVATE fmt::fmt)
-find_package(SFML COMPONENTS system window graphics CONFIG REQUIRED)
-target_link_libraries(${CLIENT_CORE_LIB} PRIVATE sfml-graphics sfml-window sfml-system)
+find_package(SFML REQUIRED COMPONENTS graphics system window CONFIG)
+target_link_libraries(${CLIENT_CORE_LIB} PUBLIC sfml-graphics sfml-window sfml-system)
 
 # Internal dependencies
 target_link_libraries(${CLIENT_CORE_LIB} PRIVATE r-type_network_sdk)
