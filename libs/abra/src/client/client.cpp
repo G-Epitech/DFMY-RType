08f92c742db8973bb78e597fbb3e3cf3f179cb19
TekMath <matheo.coquet@epitech.eu>
2024-10-03 16:05:06 +0200

    feat(abra): add boost asio package

---
+++ libs/abra/src/client/client.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** client.cpp
+** File description:
+** Client class
+*/
+
+#include "client.hpp"
+
+namespace abra {
+} // abra
---
+++ libs/abra/src/client/client.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** client.hpp
+** File description:
+** Client class
+*/
+
+#include <boost/asio.hpp>
+
+#include "api.hpp"
+
+#pragma once
+namespace abra {
+  class EXPORT_NETWORK_SDK_API Client;
+} // abra
