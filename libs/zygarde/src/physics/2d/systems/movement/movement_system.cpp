1e1217bb2909b656a002c6c11a27939ae50883a6
Flavien Chenu <flavienchenu@hotmail.com>
2024-10-12 12:20:03 +0200

    refactor(zygarde): update position storage way

--- libs/zygarde/src/physics/2d/systems/movement/movement_system.cpp
+++ libs/zygarde/src/physics/2d/systems/movement/movement_system.cpp
@@ -9,19 +9,19 @@
 
 using namespace zygarde::physics::systems;
 
-MovementSystem::MovementSystem(const utils::Timer::Nanoseconds& deltaTime)
-    : deltaTime_(deltaTime) {}
+MovementSystem::MovementSystem(const utils::Timer::Nanoseconds& delta_time)
+    : ASystem(), deltaTime_(delta_time) {}
 
 void MovementSystem::Run(std::shared_ptr<Registry> r,
                          tools::sparse_array<components::Rigidbody2D>::ptr rigidbodies,
-                         tools::sparse_array<core::components::Transform>::ptr transforms) {
-  auto max = std::max(rigidbodies->size(), transforms->size());
+                         tools::sparse_array<core::components::Position>::ptr positions) {
+  auto max = std::max(rigidbodies->size(), positions->size());
   for (size_t i = 0; i < max; ++i) {
     auto& rigidbody = (*rigidbodies)[i];
-    auto& transform = (*transforms)[i];
-    if (rigidbody && transform) {
+    auto& position = (*positions)[i];
+    if (rigidbody && position) {
       ComputePositionOffset(&(*rigidbody));
-      UpdateTransformPosition(&((*transform)));
+      UpdatePosition(&((*position)));
     }
   }
 }
@@ -38,7 +38,7 @@ void MovementSystem::ComputePositionOffset(components::Rigidbody2D* rigidbody) {
   movementOffset_.y = velocity.y * deltaTimeSec;
 }
 
-void MovementSystem::UpdateTransformPosition(core::components::Transform* transform) const {
-  transform->position.x += movementOffset_.x;
-  transform->position.y += movementOffset_.y;
+void MovementSystem::UpdatePosition(core::components::Position* position) const {
+  position->point.x += movementOffset_.x;
+  position->point.y += movementOffset_.y;
 }
