087363c6fa8350b57de4aebf9d224b5398adb065
TekMath <matheo.coquet@epitech.eu>
2024-11-02 23:22:14 +0100

    feat(monitor): init web app

---
+++ monitor/src/app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@nextui-org/button";
+
+export default function Home() {
+    return (
+        <main>
+            <p>Hey</p>
+            <Button type="submit">Click on me</Button>
+        </main>
+    );
+}
