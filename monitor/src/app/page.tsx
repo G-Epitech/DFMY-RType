5e5b5cadbc1132f64ae505e5b0d290f40ff7a34e
TekMath <matheo.coquet@epitech.eu>
2024-11-03 14:40:19 +0100

    feat(monitor): add token input

--- monitor/src/app/page.tsx
+++ monitor/src/app/page.tsx
@@ -1,10 +1,16 @@
+"use strict";
+
 import {Button} from "@nextui-org/button";
+import {Input} from "@nextui-org/input";
 
 export default function Home() {
     return (
-        <main>
-            <p>Hey</p>
-            <Button type="submit">Click on me</Button>
+        <main className="h-screen flex items-center justify-center">
+            <div className="flex gap-2 items-end">
+                <Input type="password" variant="bordered" label="Token" placeholder="Enter the master token"
+                       labelPlacement="outside"/>
+                <Button type="submit" color="success" size="md">Connect</Button>
+            </div>
         </main>
     );
 }
